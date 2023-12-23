73e55d99e9e2cb2e33a2f1e5db446ac264d47d7b
abellopmar <92862957+abellopmar@users.noreply.github.com>
2023-12-23 22:24:56 +0100

    plot bien

--- Práctica 2/Código/hunt.R
+++ Práctica 2/Código/hunt.R
@@ -118,16 +118,24 @@ create_classification = function(sample, col, right_element, criteria, measure) 
 df_2_tree = function(df, final, criteria) {
   
   last_tree = Node$new(df[nrow(df), "parent"])
+  last_tree$gain = df[nrow(df), "gain"]
+  SetNodeStyle(last_tree, label = paste(last_tree$name, "\n", round(last_tree$gain, 3)))
+  
   new_left_leaf = Node$new(paste(get_elements(final, criteria)))
   new_left_leaf$label = final[nrow(final), df[nrow(df), "parent"]]
+  new_left_leaf$gain = ""
   last_tree$AddChildNode(new_left_leaf)
   
   new_right_leaf = Node$new(df[nrow(df), "right_element"])
   new_right_leaf$label = df[nrow(df), "right"]
+  new_right_leaf$gain = ""
   last_tree$AddChildNode(new_right_leaf)
   
   SetEdgeStyle(new_left_leaf, label = new_left_leaf$label, fontsize = 10)
   SetEdgeStyle(new_right_leaf, label = new_right_leaf$label, fontsize = 10)
+  
+  SetNodeStyle(new_left_leaf, label = paste(new_left_leaf$name, "\n", new_left_leaf$gain))
+  SetNodeStyle(new_right_leaf, label = paste(new_right_leaf$name, "\n", new_right_leaf$gain))
   
   for (row_index in (nrow(df)-1):1) {
     last_tree$label = df[row_index, "left"]
@@ -136,12 +144,17 @@ df_2_tree = function(df, final, criteria) {
     
     right_node = Node$new(df[row_index, "right_element"])
     right_node$label = df[row_index, "right"]
+    right_node$gain = ""
     new_tree$AddChildNode(right_node)
     
     SetEdgeStyle(right_node, label = right_node$label, fontsize = 10)
     SetEdgeStyle(last_tree, label = last_tree$label, fontsize = 10)
     
     last_tree = new_tree
+    
+    last_tree$gain = df[row_index, "gain"]
+    SetNodeStyle(last_tree, label = paste(last_tree$name, "\n", round(last_tree$gain, 3)))
+    SetNodeStyle(right_node, label = paste(right_node$name, "\n", right_node$gain))
   }
   
   #plot(last_tree)#mirar si va en latex
@@ -190,7 +203,7 @@ hunt = function(sample, classes, criteria, measure, details = FALSE) {
 	tree = df_2_tree(final_clasification, sample, criteria)
 	
 	cat("\n\nÁrbol de decisión\n=================\n")
-	print(tree, "label")
+	print(tree, "label", "gain")
 	
 	tree
 }
