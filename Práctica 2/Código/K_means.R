39561f14ae8b3250b0bf95386ab662be34648299
abellopmar <92862957+abellopmar@users.noreply.github.com>
2023-12-23 23:20:50 +0100

    kmeans details

--- Práctica 2/Código/K_means.R
+++ Práctica 2/Código/K_means.R
@@ -68,10 +68,10 @@ printCentroids = function(df_sample, centroids, assig_matrix){
   n_pts = ncol(assig_matrix)
   cat("\n--RESULTADOS--\n")
   for (cent in 1:n_cents){
-    cat("Centroide", cent, "(", centroids[cent,1], ",", centroids[cent,2],") \n")
+    cat(paste0("Centroide ", cent, " (", centroids[cent,1], ", ", centroids[cent,2],") \n"))
     for(pt in 1:n_pts){
       if (assig_matrix[cent,pt] == 1){
-        cat("  - Punto", pt, "(", df_sample[pt, 1], ",", df_sample[pt,2], ")\n")
+        cat(paste0("  - Punto ", pt, " (", df_sample[pt, 1], ", ", df_sample[pt,2], ")\n"))
       }
     }
     cat("\n")
@@ -105,8 +105,9 @@ fcd_kmeans = function(df_sample, centroids, details = FALSE) {
     cat("PASO", step, "\n------- \n")
     cat("Matriz de distancias \n")
     print(dist_matrix)
-    cat("Matriz de asignaciones \n")
+    cat("\nMatriz de asignaciones \n")
     print(assig_matrix)
+    cat("\n")
   }
   
   while (!identical(prev_assig_matrix, assig_matrix)) {
@@ -119,11 +120,12 @@ fcd_kmeans = function(df_sample, centroids, details = FALSE) {
       cat("Los nuevos centroides son:\n")
       print(centroids)
       step = step + 1
-      cat("PASO", step, "\n------- \n")
+      cat("\nPASO", step, "\n------- \n")
       cat("Matriz de distancias \n")
       print(dist_matrix)
-      cat("Matriz de asignaciones \n")
+      cat("\nMatriz de asignaciones \n")
       print(assig_matrix)
+      cat("\n")
     }
   }
   printCentroids(df_sample, centroids, assig_matrix)
@@ -132,7 +134,7 @@ fcd_kmeans = function(df_sample, centroids, details = FALSE) {
 }
 
 
-(data = read.xlsx("data/datosKMeans.xlsx"))
-(centroides = read.xlsx("data/centroides.xlsx"))
+(data = read.xlsx("../Memoria/data/datosKMeans.xlsx"))
+(centroides = read.xlsx("../Memoria/data/centroides.xlsx"))
 
-fcd_kmeans(data, centroides)
+fcd_kmeans(data, centroides, TRUE)
